23b04615ce48862b2e9462460874b83c85e9c43b
Yigit Can Dundar <yigitcandundar@hotmail.com>
2016-01-03 21:08:15 +0200

    Added wave timers, objective text, timer text, changed AI detection cone size, Official activate/deactivate mechanics added.

---
+++ Project H. Level Design/Assets/Scripts/Scene Scripts/HideWaveScript.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HideWaveScript : MonoBehaviour {
+    
+    [SerializeField]
+    private float hideTime=30f;
+
+    private float minute;
+    private float second;
+    private float timer;
+
+    public Text timerDisplay;
+    public Text objectiveDisplay;
+
+    private bool officialSpawned = false;
+
+    
+    public GameObject Official;
+    public Transform officialPosition;
+
+    void Start()
+    {
+        Official.SetActive(false);
+    }
+	void Update () {
+        if (hideTime > 0)
+        {
+            hideTime -= 1.0f * Time.deltaTime;
+            minute = Mathf.Floor(hideTime / 60f);
+            second = (hideTime % 60f);
+            timerDisplay.text = "Time Left: " + string.Format("{0:00}:{1:00}", minute, second);
+            objectiveDisplay.text = "Objective: Hide the money until the time runs out.";
+        }
+        else
+        {
+            timerDisplay.text = "";
+            objectiveDisplay.text = "Objective: Act natural.";
+            if (officialSpawned == false)
+            {
+                Official.SetActive(true);
+                officialSpawned = true;
+            }
+        }
+	}
+}
---
+++ Project H. Level Design/Assets/Scripts/Scene Scripts/OfficialDisabler.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OfficialDisabler : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Official")
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+}
