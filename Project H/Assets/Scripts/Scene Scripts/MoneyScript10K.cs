f48985ba772242f9aed4eb34e9c599f2e5704ccc
Yigit Can Dundar <yigitcandundar@hotmail.com>
2016-01-09 18:35:26 +0200

    Couch, Window and front door colliders have been upgraded, 5 stacks of 10.000$ have been added to the first wave, Plastic box has been added to the first wave, Officials Detection Cone has been changed to better fit the first wave (other waves are still the older ones). Minor wave behavior testing was done. W.I.P. : Other waves startup money spawns, and further testing.

--- Project H/Assets/Scripts/Scene Scripts/MoneyScript10K.cs
+++ Project H/Assets/Scripts/Scene Scripts/MoneyScript10K.cs
@@ -6,15 +6,13 @@ public class MoneyScript10K : MonoBehaviour
 
     //This may be enum or something else, depending on the prefab.
     private int moneyAmount { get; set; }
-    //Right now, amount is randomized everytime.
-
-
+    //Right now, the amount is set at 10.000$.
 
     public readonly int moneyType = 10000;
 
     void Start()
     {
-        moneyAmount = 100000;
+        moneyAmount = 10000;
     }
 
     public int getMoneyAmount()
