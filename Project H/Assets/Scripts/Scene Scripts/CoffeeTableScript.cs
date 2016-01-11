d1d26442eb210a1049264fce28e702e504d12c8f
Emre Ozdemir <emrozdmr55@gmail.com>
2016-01-11 21:57:30 +0200

    Updated CoffeeTableScript, simplified it massively. "Testing."

--- Project H/Assets/Scripts/Scene Scripts/CoffeeTableScript.cs
+++ Project H/Assets/Scripts/Scene Scripts/CoffeeTableScript.cs
@@ -4,13 +4,13 @@ using System.Collections;
 public class CoffeeTableScript : MonoBehaviour
 {
 
-    public float shelfReach = 1.1f;
+    public float shelfReach = 0.8f;
 
     [SerializeField]
     private GameObject[] drawers = new GameObject[4];
 
     private Vector3[] initPos = new Vector3[4];
-    private Vector3[] initPosLocal = new Vector3[4];
+
     void Start()
     {
         int drawerCount = 0;
@@ -18,37 +18,20 @@ public class CoffeeTableScript : MonoBehaviour
         foreach (GameObject item in drawers)
         {
             initPos[drawerCount] = item.GetComponent<Transform>().position;
-            initPosLocal[drawerCount] = item.GetComponent<Transform>().localPosition;
             drawerCount++;
         }
     }
 
 
-    void Update()
+    void LateUpdate()
     {
 
         int drawerCount = 0;
 
         foreach (GameObject item in drawers) //Normally, this would be solved with Animation but I need to Bodge this so this only works in x-axis.
         {
-            if (Vector3.Distance(initPos[drawerCount], item.GetComponent<Transform>().position) > shelfReach)
-            {
-                if (initPos[drawerCount].x - item.GetComponent<Transform>().position.x > 0)
-                {
-                    Vector3 pos = initPosLocal[drawerCount];
-                    pos.x -= shelfReach;
-                    item.GetComponent<Transform>().localPosition = pos;
-                }
-                else if (initPos[drawerCount].x - item.GetComponent<Transform>().position.x < 0)
-                {
-                    Vector3 pos = initPosLocal[drawerCount];
-                    pos.x += shelfReach;
-                    item.GetComponent<Transform>().localPosition = pos;
-                }
-                item.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            item.GetComponent<Rigidbody>().position = new Vector3(Mathf.Clamp(item.GetComponent<Rigidbody>().position.x, initPos[drawerCount].x - shelfReach, initPos[drawerCount].x + shelfReach), item.GetComponent<Rigidbody>().position.y, item.GetComponent<Rigidbody>().position.z);
 
-                //drawers[drawerCount].transform.position = initPos[drawerCount];
-            }
             drawerCount++;
         }
 
