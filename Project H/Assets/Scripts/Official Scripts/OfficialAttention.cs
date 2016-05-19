fc76b7be9c8597c6bf8fb477bfbf84a175de7b1d
Emre Ozdemir <emrozdmr55@gmail.com>
2016-05-19 19:31:01 +0300

    - Implement officialAttention script. This script also utilizes the slider on the canvas.

--- Project H/Assets/Scripts/Official Scripts/OfficialAttention.cs
+++ Project H/Assets/Scripts/Official Scripts/OfficialAttention.cs
@@ -1,19 +1,38 @@
 ﻿using UnityEngine;
-
+using UnityEngine.UI;
 public class OfficialAttention : MonoBehaviour
 {
 
-    public float officialAttention { get { return officialAttention; } private set { } }
-
+    private float officialAttention;
+    public Text attentionText;
+    public Slider attentionSlider;
     void Start()
     {
+        officialAttention = 51f;
 
+        attentionText.text = "Attention";
+
+        attentionSlider.wholeNumbers = false;
+        attentionSlider.maxValue = 100;
+        attentionSlider.minValue = 0f;
+        attentionSlider.interactable = false;
+
+        attentionSlider.value = officialAttention;
     }
 
 
     void Update()
     {
-
+        attentionSlider.value = officialAttention;
     }
 
+    public float getAttentionValue()
+    {
+        return officialAttention;
+    }
+
+    public void IncrementAttention(float _value)
+    {
+        officialAttention += _value;
+    }
 }
